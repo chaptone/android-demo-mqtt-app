154d788e5c6fff446d636988b4c5fff8f014733a
Rakkan Jintasatien <rakkan.j@ku.th>
2018-06-08 10:47:52 +0200

    add-tab-activity-2

---
+++ app/src/main/java/com/example/chapmac/rakkan/android_demo_mqtt_app/Activity3.java
@@ -0,0 +1,31 @@
+package com.example.chapmac.rakkan.android_demo_mqtt_app;
+
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+
+public class Activity3 extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_3);
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Tab1Fragment(), "TAB1");
+        adapter.addFragment(new Tab2Fragment(), "TAB2");
+        viewPager.setAdapter(adapter);
+    }
+}
